1b254f6fb8bb2898bdfd23f5210544f7638960f8
Georgy Usatenko <g.usatenko@tinkoff.ru>
2023-12-11 11:50:30 +0300

    mergesort

--- drafts/sort.py
+++ drafts/sort.py
@@ -1,5 +1,7 @@
 from random import randint
 
+from py_tools.time import timing
+
 
 def qsort(arr):
     def recur(start, end):
@@ -23,21 +25,70 @@ def qsort(arr):
 
 
 def mergesort(arr):
+    def merge(start, mid, end):
+        a = arr[start:mid]
+        b = arr[mid:end]
+        p = start
+        p1 = 0
+        p2 = 0
+        a_len = len(a)
+        b_len = len(b)
+        while p1 < a_len and p2 < b_len:
+            if a[p1] < b[p2]:
+                arr[p] = a[p1]
+                p1 += 1
+            else:
+                arr[p] = b[p2]
+                p2 += 1
+            p += 1
+        while p1 < a_len:
+            arr[p] = a[p1]
+            p1 += 1
+            p += 1
+        while p2 < b_len:
+            arr[p] = b[p2]
+            p2 += 1
+            p += 1
+
     def recur(start, end):
-        if start - end > 2:  # noqa R509  #pylint:disable=no-else-raise
-            mid = (end - start) // 2
+        if end - start > 2:
+            mid = (end + start) // 2
             recur(start, mid)
             recur(mid, end)
-            raise NotImplementedError('too difficult to do in-place')  # merge
+            merge(start, mid, end)
         else:
-            if arr[start] > arr[start + 1]:
-                arr[start], arr[start + 1] = arr[start + 1], arr[start]
+            if arr[start] > arr[end - 1]:
+                arr[start], arr[start + 1] = arr[start], arr[start + 1]
 
     recur(0, len(a))
 
 
 if __name__ == '__main__':
-    a = [randint(-50, 150) for _ in range(300)]
-    print(a)
-    qsort(a)
-    print(a)
+    q_times = []
+    m_times = []
+    for _ in range(200):
+        a = [randint(0, 150000) for _ in range(30000)]
+        tmp = a[:]
+        with timing() as t:
+            mergesort(tmp)
+        m_times.append(t())
+
+        tmp = a[:]
+        with timing() as t:
+            qsort(tmp)
+        q_times.append(t())
+
+    print('merge', sum(m_times) / len(m_times))
+    print('qsort', sum(q_times) / len(q_times))
+
+"""
+merge 0.030498292117845268
+qsort 0.025943265575915576
+remove swap call
+merge 0.02992545232642442
+qsort 0.025914575769566
+remove comment and unused func
+merge 0.029527431223541498
+qsort 0.025833900645375253
+
+"""
